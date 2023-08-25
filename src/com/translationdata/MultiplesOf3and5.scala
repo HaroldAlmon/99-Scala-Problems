4d9beea5ccdcd6c409d81a7209fb6120402b1443
Harold Almon <HaroldAlmon@users.noreply.github.com>
2023-08-25 17:35:07 -0500

    Added better explanation and fixed the upperlimit to be non-inclusive.

--- src/com/translationdata/MultiplesOf3and5.scala
+++ src/com/translationdata/MultiplesOf3and5.scala
@@ -7,7 +7,7 @@ import org.junit.Assert._
 
 object MultiplesOf3and5 {
   def main(args: Array[String]) {
-    val sum = MultiplesOf3and5.multiplesOf3And5(1000)
+    val sum = MultiplesOf3and5.multiplesOf3And5(1000 - 1)
     println("P001: " + sum)
   }
 
@@ -19,12 +19,21 @@ object MultiplesOf3and5 {
 
   // This is a variation of the sum(1..n) formula.
   //
-  // Sum(1k, 2k, 3k...kn) = k * n * (n + 1)
+  // Sum(1k, 2k, 3k...nk) = k * n * (n + 1)
   //                   -----------
   //                        2
+  // Why?
+  // Sum(1k, 2k, 3k...nk) = k * Sum(1, 2, 3...n) -- Factor out the k
+  // e.g. Sum(1*3, 2*3, 3*3...n*3) = 3 * Sum(1, 2, 3...n)
+  //
+  // Sum(1, 2, 3...n) = n * (n + 1)/2 -- standard sum(1...n) formula
+  // so if we have sum(3,6,9), divide the last term by 3 (9/3 = 3), find sum(1,2,3)
+  // and multiply the result by 3.
+
   // The formula includes n, the problem does not.
-  private def calculateSumBelowLimit(upperLimit: Int, divisor: Int) = {
-    val n = (upperLimit - 1) / divisor
-    divisor * n * (n + 1) / 2
+  private def calculateSumBelowLimit(upperLimit: Int, k_value: Int) = {
+    val n_value = upperLimit / k_value
+    //val n_value = (upperLimit - 1) / k_value
+    k_value * n_value * (n_value + 1) / 2
   }
 }
